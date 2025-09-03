346e281dcdfe85da1fe231ddda2e2e13bd376b16
JitenDirisam <jiten.dirisam2006@gmail.com>
2025-09-03 19:04:15 +0530

    Add files via upload

---
+++ Basic Mathematical Operations.py
@@ -0,0 +1,21 @@
+#Taking two numbers from the user:)
+num1 = input("Enter the First Number: ")
+num2 = input("Enter the Second Number: ")
+
+num1= float(num1)
+num2= float(num2)
+#Performs the basic mathematical operations on these two numbers:)
+
+Add = num1 + num2
+
+Sub = num1 - num2
+
+Multiply = num1 * num2
+
+Division = num1/num2
+#time to print the output:)
+print("Addition: ",Add)
+print("Subtraction: ",Sub)
+print("Multiplication: ",Multiply)
+print("Division: ",Division)
+
---
+++ Personalized Greeting.py
@@ -0,0 +1,17 @@
+#Creating a personalised greeting message:)
+
+#Taking first name and Last name separately:)
+
+firstn =input("Please Enter your First Name: ")
+lastn = input("Please Enter your Last Name: ")
+
+firstn = str(firstn)
+lastn = str(lastn)
+
+#now adding this both name together:)
+
+fullname = firstn  + " " + lastn
+
+# personalised greeting message:)
+
+print("Hello,",fullname+"!"+" Welcome to The Science Exhibition")
